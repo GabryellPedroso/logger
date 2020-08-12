2fc04448e948eedb0e6695debb5fa4f671a9b10c
Gabryell Pedroso <32345769+theSpiderDev@users.noreply.github.com>
2020-08-12 12:31:31 -0300

    feat: Index file

---
+++ index.js
@@ -0,0 +1,8 @@
+const { NODE_ENV } = process.env;
+const isDevelopment = (!NODE_ENV || NODE_ENV !== 'production');
+
+export default {
+  log: text => isDevelopment && console.log(text),
+  error: text => isDevelopment && console.error(text),
+  warn = text => isDevelopment && console.warn(text),
+};
